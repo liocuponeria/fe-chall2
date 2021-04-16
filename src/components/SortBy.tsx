856ca7bd83cdcdb4ecce2c097f6c890078cdac69
Saimon Ventura <saimonventura@gmail.com>
2021-04-16 09:16:05 -0300

    fix: padding

--- src/components/SortBy.tsx
+++ src/components/SortBy.tsx
@@ -35,7 +35,7 @@ const SortByContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 30px;
+  padding: 30px 132px 30px 132px;
   flex-direction: row;
 
   span {
